882294fbfdbdc1eec27d9f54c2ffa3dd51214508
Rithwik Babu <rithwikbabu13@gmail.com>
2021-08-11 22:15:00 -0500

    Finished Product

---
+++ viewbot.py
@@ -0,0 +1,23 @@
+# Webdriver Imports
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Misc Imports
+import time
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("https://omicron.life/")
+a = driver.find_element_by_link_text("About Omicron")
+b = driver.find_element_by_link_text("Depression Detection")
+
+try:
+    while True:
+        a.click()
+        time.sleep(2)
+        b.click()
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("Bot Stopped")
+    pass
